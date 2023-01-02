5fe41f8569ebf9d772fb659d3e92f4f4aa7c8764
Andrzej Szczap <andrzejandrejoo@gmail.com>
2023-01-02 02:22:15 +0100

    Added ControllerAdvice to handle the NoSuchElementException. Added methods to in api to create, get, update and delete Employee.

--- SpringBackend/src/main/java/com/example/springbackend/controller/EmployeeController.java
+++ SpringBackend/src/main/java/com/example/springbackend/controller/EmployeeController.java
@@ -4,9 +4,7 @@ import com.example.springbackend.model.Employee;
 import com.example.springbackend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,7 +19,28 @@ public class EmployeeController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Employee>> getEmployees(){
+    public ResponseEntity<List<Employee>> getEmployees() {
         return new ResponseEntity<>(employeeService.getEmployees(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(employeeService.getEmployeeById(id), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.addEmployee(employee), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> updateEmployeeById(@PathVariable("id") Long id, @RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.updateEmployeeById(id, employee), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEmployeeById(@PathVariable("id") Long id) {
+        employeeService.deleteEmployeeById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- SpringBackend/src/main/java/com/example/springbackend/service/EmployeeService.java
+++ SpringBackend/src/main/java/com/example/springbackend/service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.example.springbackend.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class EmployeeService {
@@ -18,4 +19,40 @@ public class EmployeeService {
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
     }
+
+    public Employee getEmployeeById(Long id) {
+        return findEmployeeById(id);
+    }
+
+    private Employee findEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElseThrow(
+                () -> new NoSuchElementException("There is no employee with id: " + id));
+    }
+
+    public Employee addEmployee(Employee employee){
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployeeById(Long id, Employee employee) {
+        Employee toUpdate = getEmployeeById(id);
+        if (employee.getFirstName() != null) {
+            toUpdate.setFirstName(employee.getFirstName());
+        }
+        if (employee.getLastName() != null) {
+            toUpdate.setLastName(employee.getLastName());
+        }
+        if (employee.getEmail() != null) {
+            toUpdate.setEmail(employee.getEmail());
+        }
+        if (employee.getSalary() != null) {
+            toUpdate.setSalary(employee.getSalary());
+        }
+        return employeeRepository.save(toUpdate);
+    }
+
+    public void deleteEmployeeById(Long id){
+        employeeRepository.deleteById(id);
+    }
+
+
 }
