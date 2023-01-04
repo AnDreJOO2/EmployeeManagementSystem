2b258691c37e94508a571d37a35c2edefcfdd4fa
Andrzej Szczap <andrzejandrejoo@gmail.com>
2023-01-04 23:27:51 +0100

    Added needed components.

--- AngularFrontend/src/app/app.module.ts
+++ AngularFrontend/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeePageComponent } from './components/employee-page/employee-page.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { DeleteEmployeeComponent } from './components/delete-employee/delete-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeePageComponent,
+    EmployeeComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    DeleteEmployeeComponent
   ],
   imports: [
     BrowserModule
