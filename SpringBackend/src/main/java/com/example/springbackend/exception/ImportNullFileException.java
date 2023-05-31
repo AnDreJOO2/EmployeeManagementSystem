2e22f8095395b4bbd3d600fb62bb2ef3c71fb1f5
Andrzej Szczap <andrzejandrejoo@gmail.com>
2023-05-31 19:33:43 +0200

    Deleted non used Exception.

--- SpringBackend/src/main/java/com/example/springbackend/exception/ImportNullFileException.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbackend.exception;
-
-public class ImportNullFileException extends RuntimeException {
-
-    public ImportNullFileException() {
-        super("Imported file can't be null");
-    }
-}
